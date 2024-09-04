034e4fe198caa2c0134fa90c9829362771ec0f31
Shalomiii <samsomshalom@yahoo.com>
2024-09-04 00:35:34 -0700

    worked on implementing user login and signup functionalities

---
+++ main_app/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE) 
+
+    def __str__(self):
+        return self.name
+
+class Todo_Task(models.Model):
+    project = models.ForeignKey(Project, related_name='todo_tasks', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Suggestion(models.Model):
+    Todo_Task = models.ForeignKey(Todo_Task, related_name='suggestions', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Suggestion by {self.user} on {self.Todo_Task.title}'
---
+++ main_app/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('login')
+    else:
+        form = UserCreationForm();
+    
+    return render(request, 'registration/signup.html', {'form': form})
+
+def home(request):
+    return render(request, 'page/home.html')
+
+@login_required
+def profile(request):
+    user = request.user
+    return render(request, 'registration/profile.html', {'user': user})
